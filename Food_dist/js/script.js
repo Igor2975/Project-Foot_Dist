6f8617e7a4fc19e4ca3f532bd123517dcc8b4d2b
Igor2975 <89157738+Igor2975@users.noreply.github.com>
2022-06-03 21:55:21 +0300

    Добавление классов через spred оператор и по умолчанию

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -147,13 +147,14 @@ window.addEventListener('DOMContentLoaded', () => {
     // используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector,...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
             this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
             this.transfer = 30;
             this.changeToRub();
         }
@@ -164,8 +165,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             let elem = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.el = 'menu__item';
+                elem.classList.add(this.el)
+            } else {
+                this.classes.forEach(className => {
+                    elem.classList.add(className)
+                })
+            }
             elem.innerHTML = `
-            <div class="menu__item">
                 <img src= ${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -174,7 +182,6 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
                 </div>
-            </div>
             `
             this.parent.append(elem)
         }
